8309c8abd810e218dfd10eac27a3a376eac2787b
enti <enti@enti.enti>
2022-10-26 10:30:34 +0200

    Juego de adivinar el num

--- adivina.sh
+++ adivina.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+clear
+NUMERO_RANDOM=$(( $RANDOM % 100 + 1))
+
+while [[ $NUM -ne $NUMERO_RANDOM ]]
+do
 echo "Adivina el numero del 1 al 100"
+
 read NUM
-IF [ "$NUM" -eq "3" ]
+
+if [[ $NUM -gt $NUMERO_RANDOM ]]
+then
+	echo "Es menor"
+elif [[ $NUM -lt $NUMERO_RANDOM ]]
+then
+	echo "Es mayor"
+else
+	echo "Son iguales"
+fi
+done
